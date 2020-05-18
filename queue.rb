9897e7c708d0b9b25369e932d21cbe2ca762c20d
forison <forison6205@gmail.com>
2020-05-18 10:51:10 -0400

    added queue class implementation

---
+++ queue.rb
@@ -0,0 +1,18 @@
+class Queue
+	def initialize(stack=[])
+		@stack = stack
+	end
+	def enque(arg)
+		@stack<<arg
+	end
+	def dequeue
+		@stack.delete_at(0)
+	end
+end
+
+	 q = Queue.new
+	 q.enque(23)
+	 q.enque(27)
+	 q.enque(2)
+	 q.dequeue
+	 p q.dequeue
