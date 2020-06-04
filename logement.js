cf8044ecb5a3515ec2ea1882f8de4d78282ac267
forison <forison6205@gmail.com>
2020-06-04 11:23:00 -0400

    add reimplimentation graph emulator and coordplotter

--- logement.js
+++ logement.js
@@ -2,36 +2,46 @@ function point(x,y) {
   return {x,y}
 }
 // This function should return the closest point to "pos" that is inside the polygon defined by "poly" 
+const positonTracker = {}
 function getClosestPointInsidePolygon(poly, pos){
   graphEmulator();
   coordPlotter(poly,pos);
   allX(poly);
   allY(poly);
-  console.log(detLocOfPos(poly, pos));
+  detLocOfPos(poly, pos)
 }
   
 //This should also work with shapes more complex than a square.  
 // example output:
 getClosestPointInsidePolygon(
     [ { x: 0, y: 0 }, { x: 100, y: 0 }, { x: 100, y: 100 }, { x: 0, y: 100 } ],
-     { x: -150, y: 5 }
+     { x: 150, y: 50 }
 );
 // example output: 
 //should return { x: 100, y: 50 }
 
 function graphEmulator(){
-  const graphSheet = new Array(350);
-  for (let index = 0; index <= 350; index++) {
-    graphSheet[index]= new Array(350);
+  const graphSheet = new Array(150);
+  for (let index = 0; index <= 150; index++) {
+    graphSheet[index]= new Array(150);
   }
-  for (let i = 0; i < graphSheet.length; i++) {
-    for (let j = 0; j < graphSheet.length; j++) {
-      graphSheet[i][j]=`${i},${j}`;
+  let k = 74;
+  let y = 0;
+  let x = 0;
+  for (let i = 0; i <= 150; i++) {
+    for (let j = 0; j <= 150; j++) {
+      y = k - 150
+      k - 150 >= 1 ? x = i : x = i * -1;
+      k++
+      graphSheet[i][j]= `${x},${y}`;
+      positonTracker[`${x},${y}`] = `${i},${j}`;
     }
+    k = 74;
+    y=0;
+    x=0;
   }
   return graphSheet;
 }
-
 
 function coordPlotter(poly=[], pos){
  poly.push(pos);
@@ -39,7 +49,7 @@ function coordPlotter(poly=[], pos){
  for (let i = 0; i < poly.length; i++) {
    let coordX = poly[i].x;
    let coordY = poly[i].y;
-   sheet[coordX][coordY]='X';
+   sheet[coordX][coordY]= 'X';
  }
  return sheet
 }
